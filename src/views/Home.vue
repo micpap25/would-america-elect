bfcf561740801fa5ea018d8bb5818022f95016c2
Michael Papadopoulos <micpap25@gmail.com>
2020-10-28 00:22:45 -0400

    NPM updates and home page blurb

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="home">
-
+    <h1>Would America Elect...?</h1>
+    <p>
+      In January 2020, a Gallup poll surveyed 1,033 people across the United
+      States of America, asking the following question:
+    </p>
+    <h3>
+      Between now and the 2020 political conventions, there will be discussion
+      about the qualifications of presidential candidates -- their education,
+      age, religion, race, and so on. If your party nominated a generally
+      well-qualified person for president who happened to be [characteristic],
+      would you vote for that person?
+    </h3>
+    <p>
+      These characteristics included race, religion, age, and other major
+      aspects that influence how the voting public views a candidate. This quiz
+      will ask you to guess what percent of Americans responded "Yes" to each
+      characteristic. Afterwards, you can compare your answers to the poll's
+      results. By taking this quiz, you'll walk away with a lot more information
+      on America's voting tendencies.
+    </p>
   </div>
 </template>
 
